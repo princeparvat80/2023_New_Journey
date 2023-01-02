39cc01023a37340571d7c37da32b92221e16381e
princeparvat80 <princeparvat80@gmail.com>
2023-01-02 23:28:15 +0530

    created our own server with Parcel

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
 const heading = React.createElement("h1", {}, "Prince");
 const heading2 = React.createElement("h2", {}, "Parvat");
 const container = React.createElement("div", {}, [heading, heading2]);
