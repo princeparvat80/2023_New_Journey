b6ebfca051e68d27ebd29909a720a8c1253ce1d3
princeparvat80 <princeparvat80@gmail.com>
2023-01-02 21:47:18 +0530

    multiple HTML eLements

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {}, "Prince");
+const heading2 = React.createElement("h2", {}, "Parvat");
+const container = React.createElement("div", {}, [heading, heading2]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
